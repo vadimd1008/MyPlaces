e008187f4e7668320a1fc1255fce73f82ebc5e39
Vadim Dmitriev <lenta50000@gmail.com>
2020-09-03 18:25:20 +0300

    Add New Place

--- MyPlaces/TableViewController.swift
+++ MyPlaces/TableViewController.swift
@@ -55,5 +55,6 @@ class TableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBAction func cancelAction(_ segue: UIStoryboardSegue) {}
 
 }
