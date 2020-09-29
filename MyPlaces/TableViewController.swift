04b83e61898bd4aa93cf9ee0d6c41705924a8267
Vadim Dmitriev <lenta50000@gmail.com>
2020-09-29 14:27:09 +0300

    Rating Control

--- MyPlaces/TableViewController.swift
+++ MyPlaces/TableViewController.swift
@@ -75,6 +75,10 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
     
     // MARK: - Table view delegate
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             let place = places[indexPath.row]
